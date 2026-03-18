fe6808ae8e0b4278fe7df7c1f72448a4d480224d
skowas <skoalmashouq@gmail.com>
2026-03-18 05:23:53 +0300

    fix nvidia egl for star citizen

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,12 +1,18 @@
 { config, pkgs, ... }:
-
 {
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = [ pkgs.nvidia-vaapi-driver ];  # add this
+    extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+      vulkan-loader
+      vulkan-validation-layers
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      vulkan-loader
+    ];
   };
 
   hardware.nvidia = {
@@ -14,5 +20,18 @@
     modesetting.enable = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     powerManagement.enable = true;
+    nvidiaSettings = true;
+  };
+
+  # Fix EGL for NVIDIA under Wayland/Wine
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    # Force NVIDIA EGL
+    __EGL_VENDOR_LIBRARY_FILENAMES = "${pkgs.mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json:${config.hardware.nvidia.package}/share/glvnd/egl_vendor.d/10_nvidia.json";
   };
 }
